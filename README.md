0415e34ddf0a89835016e642703c7f42d6b08ed6
balu-pinisety <manikantapandu225@gmail.com>
2021-07-22 23:52:13 +0530

    Added: File for Cab Service Invoice Program

---
+++ README.md
@@ -0,0 +1,4 @@
+# Cab Invoice Service
+* Program for the Creation of Cab Invoice service
+* Ability to Calculate Fare for a Ride
+* Ability to gets the list of Rides
---
+++ src/main/java/com/CabService/CabMain.java
@@ -0,0 +1,18 @@
+package com.CabService;
+
+/**
+ * @author BALU
+ * Main class for Cab Service Program
+ * Cab Invoice service to calculate invoice for given user and ride type
+ */
+public class CabMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Welcome Message
+		System.out.println("Program for the Cab Service");
+	}
+
+}
